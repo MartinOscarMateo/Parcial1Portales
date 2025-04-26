d04c6197040dee199d5fa54ebb4fbe212f833512
DanaMichelleZ <dana.zambelli@davinci.edu.ar>
2025-04-26 14:43:28 -0300

    com

---
+++ resources/views/forum.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Novedades')
+
+@section('content')
+    <h1 class="mb-4">Novedades y Noticias</h1>
+
+    <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action">
+            Nueva colección de verano 2025 disponible ahora.
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            ¡Grandes descuentos este fin de semana!
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            Abrimos nueva sucursal en Mendoza.
+        </a>
+    </div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,8 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda de Ropa</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+@extends('layouts.app')
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
-    <a class="navbar-brand" href="/">Mi Tienda</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="/products">Productos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/contact">Contacto</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/forum">Novedades</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+@section('title', 'Inicio')
 
+@section('content')
 <section class="bg-primary text-white text-center p-5">
   <div class="container">
     <h1 class="display-4">¡Bienvenido a Mi Tienda de Ropa!</h1>
@@ -37,8 +10,4 @@
     <a href="/products" class="btn btn-light btn-lg mt-3">Ver productos</a>
   </div>
 </section>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-</body>
-</html>
+@endsection
